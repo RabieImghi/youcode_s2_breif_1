09256711cda32c94b8e720f7980f70f44b4a804c
RabieImghi <rabieaitimghi7@gmail.com>
2023-11-15 14:11:37 +0100

    finalise la premier user storypour ajoute nouvau ustilisateur

--- script.sql
+++ script.sql
@@ -1,6 +1,6 @@
 --
 -- Base de données : `SystemeGestionBreifs1`
-CREATE DATABASE SystemeGestionBreifs1;
+CREATE DATABASE IF NOT EXISTS SystemeGestionBreifs1;
 
 USE SystemeGestionBreifs1;
 --
@@ -9,7 +9,7 @@ USE SystemeGestionBreifs1;
 --
 -- Create la table utilisateur de base donnes 
 
-CREATE TABLE Utilisateur(
+CREATE TABLE IF NOT EXISTS  Utilisateur(
     `UserID` INT PRIMARY KEY AUTO_INCREMENT,
     `userName` VARCHAR(50),
     `email` VARCHAR(50)
@@ -18,7 +18,7 @@ CREATE TABLE Utilisateur(
 --
 -- Create la table Squad de base donnes 
 
-CREATE TABLE Squad(
+CREATE TABLE IF NOT EXISTS Squad(
     `SquadID` INT PRIMARY KEY AUTO_INCREMENT,
     `SquadNAME` VARCHAR(50),
     `ProjectID` INT,
@@ -27,7 +27,7 @@ CREATE TABLE Squad(
 --
 -- Create la table Projet de base donnes 
 
-CREATE TABLE Projet(
+CREATE TABLE IF NOT EXISTS Projet(
     `ProjectID` INT PRIMARY KEY AUTO_INCREMENT,
     `ProjectName` VARCHAR(100),
     `ProjectDescription` VARCHAR(100),
@@ -37,7 +37,7 @@ CREATE TABLE Projet(
 --
 -- Create la table Ressource de base donnes  
 
-CREATE TABLE Ressource(
+CREATE TABLE IF NOT EXISTS Ressource(
     `ResourceID` INT PRIMARY KEY AUTO_INCREMENT,
     `ResourceName` VARCHAR(50),
     `CategoryID` INT,
@@ -48,14 +48,29 @@ CREATE TABLE Ressource(
 --
 -- Create la table Category de base donnes  
 
-CREATE TABLE Category(
+CREATE TABLE IF NOT EXISTS Category(
     `CategoryID` INT PRIMARY KEY AUTO_INCREMENT,
     `CategoryDescription` VARCHAR(100)
 );
 -- Create la table Subcategory de base donnes  
 
-CREATE TABLE Subcategory(
+CREATE TABLE IF NOT EXISTS Subcategory(
     `SubcategoryID` INT PRIMARY KEY AUTO_INCREMENT,
     `SubcategoryDescription` VARCHAR(100)
 );
 
+/* En tant qu'administrateur système, je veux créer de nouveaux 
+utilisateurs dans la base de données pour maintenir une liste
+actualisée des membres de l'équipe.*/
+
+DELIMITER //
+
+CREATE PROCEDURE IF NOT EXISTS InsertUtilisateur(IN p_userName VARCHAR(255), IN p_email VARCHAR(255))
+BEGIN
+    INSERT INTO Utilisateur (userName, email)
+    VALUES (p_userName, p_email);
+END //
+
+DELIMITER ;
+
+CALL InsertUtilisateur('User Name','username@gmail.com');
